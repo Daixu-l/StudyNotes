d48e42f9ab207ba1e87185e10b07ed80aa1d4fbd
daniel-long <daniel@arbfintech.com>
2019-09-11 00:24:43 +0800

    study

--- src/main/java/subject/删除链表中的节点_237.java
+++ src/main/java/subject/删除链表中的节点_237.java
@@ -30,4 +30,17 @@ public class 删除链表中的节点_237 {
 不要从你的函数中返回任何结果。
 
      */
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
 }
